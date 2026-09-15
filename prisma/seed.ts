81712fede2f623c854b87b77b6fd227d1f5ccd08
JeparaDigital <junaidigamong@gmail.com>
2026-09-15 16:11:42 +0700

    Update seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -181,13 +181,11 @@ async function main() {
   const hashed = await bcrypt.hash(adminPassword, 10);
 
   console.log(`Seeding admin account (${adminEmail})...`);
-  await prisma.admin.upsert({
-    where: { email: adminEmail },
-    update: {},
-    create: {
-      email: adminEmail,
-      password: hashed,
-      name: "Admin Jepara Digital Furniture",
+  update: {
+  password: hashed,
+  name: "Admin Jepara Digital Furniture",
+},
+create: {
     },
   });
 
